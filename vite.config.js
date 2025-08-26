4eaee5f07373829ea7e56528fab98bbe604eb268
muqorrobin22 <achmadmuqorrobinassholih@gmail.com>
2025-08-26 12:06:29 +0700

    Vite config adjustment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/Frontend-Mentor-Blog-Preview-Card/',
-  plugins: [
-		react(),
-		tailwindcss(),
-	],
+  plugins: [react(), tailwindcss()],
 })
